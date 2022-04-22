e6c972ea60fe179d25bac1a991e1057cbfb7765f
Amr Mahmoud (AAkaliT2) <amr12149@yahoo.com>
2022-04-22 17:05:03 +0200

    created a single instance of axiosRequset to make all my API Reqeusts vai it (#1)

---
+++ src/helpers/functions.js
@@ -0,0 +1,16 @@
+export function convertDate(date) {
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth() + 1).toString();
+  var dd = date.getDate().toString();
+
+  var mmChars = mm.split("");
+  var ddChars = dd.split("");
+
+  return (
+    yyyy +
+    "-" +
+    (mmChars[1] ? mm : "0" + mmChars[0]) +
+    "-" +
+    (ddChars[1] ? dd - 1 : "0" + ddChars[0] - 1)
+  );
+}
